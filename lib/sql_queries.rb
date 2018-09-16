5908de74e0b57d8a2aa4c280903eb65052b0605f
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-16 05:52:51 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) REST(projects.funding_goal) AS amount_over_goal
+"SELECT projects.title, SUM((pledges.amount)-projects.funding_goal) AS amount_over_goal
 JOIN pledges
 ON pledges.project_id = projects.id
 WHERE SUM(projects.funding_goal) > projects.funding_goal"
