b013a0fb8bf5d41794b5f02712323f7d9e63c977
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-16 06:02:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ HAVING amount_over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, SUM(pledges.amount) FROM users 
+  "SELECT users.name, SUM(pledges.amount) FROM users
   JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY pledges.amount;"
